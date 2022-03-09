9f726c3b26cd26bfb5002fee7f03f2d9508ba952
ablaamim <aku-no-hana@outlook.fr>
2022-03-09 01:55:39 +0100

    asm

---
+++ src/main.asm
@@ -0,0 +1,78 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    main.asm                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/03/09 01:40:44 by ablaamim          #+#    #+#              #
+#    Updated: 2022/03/09 01:54:30 by ablaamim         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+org 0x7C00
+bits 16
+
+
+%define ENDL 0x0D, 0x0A
+
+
+start:
+    jmp main
+
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si
+    ret
+
+
+main:
+    ; setup data segments
+    mov ax, 0           ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
+
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
+
+    ; print hello world message
+    mov si, msg_hello
+    call puts
+
+    hlt
+
+.halt
+    jmp .halt
+
+
+
+msg_hello: db 'Hello world!', ENDL, 0
+
+
+times 510-($-$$) db 0
+dw 0AA55h
